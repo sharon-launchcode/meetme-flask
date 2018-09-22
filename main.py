f66d5921c43f8c9356a4390a44760bff6a5c528c
Phyllis Scheidt <phyllisscheidt@Phyllis-Scheidts-iMac.local>
2018-09-22 19:27:00 -0400

    killed duplicate port path

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = '''
+form = """
 <!doctype html>
+<html>
     <body>
-        <form>
-            <label for="first-name">First Name</label>
+        <form action="/hello" method="post">
+            <label for="first-name">First Name:</label>
             <input id="first-name" type="text" name="first_name" />
             <input type="submit" />
         </form>
     </body>
 </html>
-
-
-'''
-
+"""
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
 
-app.run()    
+@app.route("/hello", methods=['POST'])
+def hello():
+    first_name = request.form['first_name']
+    return '<h1>Hello, ' + first_name + '</h1>'
+
+
+app.run()
